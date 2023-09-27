078aea1a4d39a252b08099d3eb9045f4f7b6de09
Tosking <tosking@duck.com>
2023-09-27 20:23:40 +0500

    added register

---
+++ src/components/Register/RegisterForm.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Input from './Input';
+
+class RegisterForm extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div className="register__form">
+                <Input/>
+            </div>
+        );
+    }
+}
+ 
+export default RegisterForm;
