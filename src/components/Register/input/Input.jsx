483ababbb0e26beac98278931e63b1baf5afe907
ZkunCry <aksentev.03@mail.ru>
2023-10-02 18:42:40 +0500

    added animation for input and replace height on min-height in register block

--- src/components/Register/input/Input.jsx
+++ src/components/Register/input/Input.jsx
@@ -2,7 +2,13 @@ import React  from "react";
 import './Input.css'
 
 const  Input=(props)=> {
-  return <input type="text" className={props.className} placeholder={props.placeholder}/>;
+  return (
+      <div className="form-group">
+      <input type="text" className={props.className} placeholder={props.placeholder}/>
+      <label className="authtorization-form__label"  htmlFor={props.className}>{props.placeholder}</label>
+      </div>
+  
+  );
 
 }
 
